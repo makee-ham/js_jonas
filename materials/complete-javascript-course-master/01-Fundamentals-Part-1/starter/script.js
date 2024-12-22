60f0f7fc46e5aea18030cdaaa282fb60a21ef6ac
Makee Ham <ina0312@naver.com>
2024-12-22 20:35:45 +0900

    241222

--- materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ materials/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,35 @@
+/*
 let js = 'amazing';
-if (js === 'amazing') alert("JavaScript is FUN!"");
 
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = "South Korea";
+let continent = "Asia";
+let population = 52;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+*/
+let age = 30;
+age = 31;
