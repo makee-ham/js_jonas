6a617189c806f8e206448b9664dfa03be75bb355
Makee Ham <ina0312@naver.com>
2024-12-24 21:00:55 +0900

    241224

--- materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/241224.js
+++ materials/complete-javascript-course-master/02-Fundamentals-Part-2/starter/241224.js
@@ -1,12 +1,12 @@
 // Activating Strict Mode
 'use strict';
 
-let hasDriversLicens = false;
+let hasDriversLicense = false;
 const passText = true;
 
 // if (passText) hasDriverLicense = true;
 if (passText) hasDriversLicense = true;
-if (hasDriversLicens) console.log("I can drive :D");
+if (hasDriversLicense) console.log("I can drive :D");
 
 // const interface = 'Audio';
 // const private = 534;
@@ -24,7 +24,7 @@ logger();
 logger();
 
 function fruitProcessor(apples, oranges) {
-	const juice = `Juice with ${apples}` apples and ${oranges} oranges.;
+	const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
 	return juice;
 }
 
@@ -36,3 +36,51 @@ const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
 
 const num = Number('23');
+
+
+// Function Declaration
+function calcAge1(birthYear) {
+	return 2037 - birthYear;
+}
+
+const age1 = calcAge1(1991);
+
+
+// Function Expression
+const calcAge2 = function (birthYear) {
+	return 2037 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+
+// Arrow function
+const calcAge3 = birthYear => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+	const age = 2037 - birthYear;
+	const retirement = 65 - age;
+	// return retirement;
+	return `${firstName} retires in ${retirement} years`;
+}
+
+console.log(yearsUntilRetirement(1991, 'Jonas'));
+console.log(yearsUntilRetirement(1980, 'Bob'));
+
+
+//
+function cutFruitPieces(fruit) {
+	return fruit * 4;
+}
+
+function fruitProcessor2(apples, oranges) {
+	const applePieces = cutFruitPieces(apples);
+	const orangePieces = cutFruitPieces(oranges);
+
+	const juice = `Juice with ${applePieces} piece of apples and ${orangePieces} pieces of oranges.`;
+	return juice;
+}
+console.log(fruitProcessor2(2, 3));
