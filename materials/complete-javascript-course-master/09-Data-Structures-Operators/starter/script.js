0c1d0507a4974269aa6cccaefeb541ac4c3cd4b1
Makee Ham <ina0312@naver.com>
2025-01-06 16:53:44 +0900

    250106

--- materials/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ materials/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -30,6 +30,21 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -46,6 +61,48 @@ const restaurant = {
   },
 };
 
+restaurant.orderDelivery({
+  time: '22:30',
+  address: 'Via del Sole, 21',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: 'Via del Sole 21',
+  starterIndex: 1,
+});
+
+// Desctructuring Objects
+const { name, openingHours, categories } = restaurant;
+console.log(name, openingHours, categories);
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+// Default values
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starterMenu);
+
+// Mutating variables
+let a = 111;
+let b = 999;
+const obj = { a: 23, b: 7, c: 13 };
+({ a, b } = obj); // {}로 시작하면 'code 블록'을 기대하므로 ()로 감싸줌!
+console.log(a, b);
+
+// Nested objects
+const {
+  fri: { open: o, close: c },
+} = openingHours;
+console.log(o, c);
+
+/*
+////////////////////////////////////////////////////
 // Destructuring Arrays
 const arr = [2, 3, 4];
 const a = arr[0];
@@ -54,7 +111,30 @@ const c = arr[2];
 
 const [x, y, z] = arr; // destructuring assignment
 console.log(x, y, z);
-console.log(arr); // 원래 배열은 영향받지 아니함
+console.log(arr); // 기존 배열은 영향받지 아니함
 
-const [first, , second] = restaurant.categories; // 두 번째 요소 스킵
-console.log(first, second);
+let [main, , secondary] = restaurant.categories; // 두 번째 요소 스킵
+console.log(main, secondary);
+
+// Switching variables
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// Receive 2 return values from a function
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// Nested destructuring
+const nested = [2, 4, [5, 6]];
+// const [i, , j] = nested;
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
+console.log(p, q, r);
+*/
