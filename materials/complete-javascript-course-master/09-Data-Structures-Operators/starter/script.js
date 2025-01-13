bf7284ea65d854fdae604dfe50f34fe778462e46
Makee Ham <ina0312@naver.com>
2025-01-13 17:51:37 +0900

    250113

--- materials/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ materials/complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -70,6 +70,45 @@ const restaurant = {
   },
 };
 
+// Working with Strings Part 1
+
+/*
+/////////////////////////////////////////////////////
+// Map Iteration
+const question = new Map([
+  ['question', 'What is the best programming language in the world?'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct!'],
+  [false, 'Try again!'],
+]);
+console.log(question);
+
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+// Quiz app
+console.log(question.get('question'));
+for (const [key, value] of question) {
+  if (typeof key === 'number') console.log(`Answer ${key} : ${value}`);
+}
+const answer = Number(prompt('Your answer'));
+console.log(answer);
+
+console.log(question.get(question.get('correct') === answer));
+
+// Convert map to array
+console.log([...question]);
+// console.log(question.entries());
+console.log(...[question.keys()]);
+console.log(...[question.values()]);
+*/
+
+/*
 //////////////////////////////////////////
 // Maps
 // 객체와 다르게, Map은 모든 타입을 key값으로 가질 수 있다.
@@ -110,6 +149,7 @@ console.log(rest.get(arr));
 // 무려 html elements도 된다
 rest.set(document.querySelector('h1'), 'Heading');
 console.log(rest);
+*/
 
 /*
 //////////////////////////////////////////
