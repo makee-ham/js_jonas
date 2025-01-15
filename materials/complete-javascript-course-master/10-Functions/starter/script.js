aef1295ba52584fa522932ab433e59e5a6d76770
Makee Ham <ina0312@naver.com>
2025-01-15 18:10:24 +0900

    250115

--- materials/complete-javascript-course-master/10-Functions/starter/script.js
+++ materials/complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,3 +1,55 @@
 'use strict';
 
+/*
 // Default Parameters
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengers, // flightNum = 'LH123'가 아니라 booking.flightNum = 'LH123'
+    price, // {'aaa', 2, 800}가 아니라 {flightNum: 'aaa', numPassengers: 2}
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('aaa', 2, 800);
+createBooking('bbb', 2);
+createBooking('ccc', 5);
+
+createBooking('ddd', undefined, 1000);
+*/
+///////////////////////////////////////////////////////
+
+// Value vs. Reference - How passing arguments works
+const flight = 'LH234';
+const jonas = {
+  name: 'Jonas Schmedtmann',
+  passport: 24739479284,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 24739479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
